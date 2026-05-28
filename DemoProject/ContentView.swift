a969cdfd48e4d44ed2b441af23b9df15af22f364
Muhammad Zohaib <m.zohaibhassan01@gmail.com>
2026-05-28 16:47:38 +0500

    Initial Commit

---
+++ DemoProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoProject
+//
+//  Created by MUHAMMAD ZOHAIB on 5/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoProject/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectApp.swift
+//  DemoProject
+//
+//  Created by MUHAMMAD ZOHAIB on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
